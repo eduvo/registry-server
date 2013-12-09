992d9d224c8aca5934c39e9a49f06e91219b1668
hSATAC <hsatac@gmail.com>
2013-12-09 15:22:37 +0800

    Add template

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "github.com/codegangsta/martini"
+import (
+	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini-contrib/render"
+)
 
 func main() {
 	m := martini.Classic()
-	m.Get("/", func() string {
-		return "Hello world!"
+
+	m.Use(render.Renderer())
+
+	m.Get("/sign_in", func(r render.Render) {
+		r.HTML(200, "login", nil)
 	})
+
 	m.Run()
 }
